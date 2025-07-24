5c68c6ad0e3e3593448be4956dcc572a02fc555f
scanfasri <apannem@charlotte.edu>
2025-07-24 16:20:57 -0400

    Add generated JavaDoc documentation to docs folder

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"RectangleTest","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"RectangleTest","l":"RectangleTest()","u":"%3Cinit%3E()"}];updateSearchResults();
